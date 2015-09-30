5cd7ba3c41408573784526f4d9a0157489525b26
DWboutin <mikaelboutin.dw@gmail.com>
2015-09-30 17:03:47 -0400

    Removing unused console.log

--- src/reducers/status-reducers.js
+++ src/reducers/status-reducers.js
@@ -6,7 +6,6 @@ export function status(state = initialState, action = '') {
   switch (action.type) {
 
     case ACTIONS.CHANGE_MONEY:
-      console.log(action.ammount);
       return Object.assign({}, state, {
         money: state.money - action.amount
       });
