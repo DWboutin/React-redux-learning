44f9ef596d13c7fb9b81725c10580f71658ccf9e
Mikaël Boutin <mboutin2@aac005.crakmedia.lan>
2015-09-30 09:54:52 -0400

    Gulp now reload server

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,50 @@
+require('babel/register')({
+  stage: 0
+});
 //http://egorsmirnov.me/2015/05/25/browserify-babelify-and-es6.html
+//http://jpsierens.com/tutorial-livereload-nodemon-gulp/
 var gulp = require("gulp");
+var express = require('express');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var nodemon = require('gulp-nodemon');
+var notify = require('gulp-notify');
+var livereload = require('gulp-livereload');
+
+var APP_PORT = 3000;
+
+function startServer() {
+  livereload.listen();
+  // configure nodemon
+  nodemon({
+    // the script to run the app
+    script: './index.js',
+    ext: 'js'
+  }).on('restart', function(){
+    // when the app has restarted, run livereload.
+    gulp.src('./index.js')
+      .pipe(livereload())
+      .pipe(notify('Reloading page, please wait...'));
+  })
+}
 
 gulp.task('build', function () {
   return browserify({entries: './src/client/app.js', extensions: ['.js'], debug: true})
     .transform(babelify)
     .bundle()
-    .pipe(source('app.js'))
+    .pipe(source('app.min.js'))
+    .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('watch', ['build'], function () {
+gulp.task('watch', function () {
   gulp.watch('./src/client/*.js', ['build']);
-  var server = require('./index');
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch'], function() {
+  startServer();
+});
